b35e1cb0647e1d7eaa1bc03d44dd056e6939d2f6
ZoeElizabeth <zoe.gelech@gmail.com>
2018-11-01 22:51:33 +0000

    fixed code that was delted

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -6,33 +6,16 @@ class MessageList extends Component {
 
 render(){
 
-
-
-
-  // console.log(this.props.newItems)
   const messages = this.props.messages.map(message =>
      
   <Message
     key= { message.id }
-    // username={ message.username }
-    // content={ message.content } 
     message = {message}
     />
   );
- 
-// const notification = this.props.messages.map(message => 
 
-//   <Notification
-//    key= { message.id }
-//    content={ message.content } 
-//    />
-// );
 
 return (
-
-//make if statements here
-
-
 
 <main className="messages">
 
